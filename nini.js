79e1aa51fc21e92612d2a0e17ff90433689213f7
bbuenosdias <angelgaliana352@gmail.com>
2025-10-20 10:35:21 +0200

    Add files via upload

---
+++ nini.js
@@ -0,0 +1,8 @@
+
+    function nini() {
+        let n = prompt("¿Eres un nini?");
+        if (n != "asdabdkabdhsnmcbmhag"){
+        alert ("nini");
+        }
+         window.location.href = "https://www.infojobs.net/";
+    }
